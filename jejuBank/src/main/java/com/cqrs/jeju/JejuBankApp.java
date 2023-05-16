1c49035cc814489424fbe3495104aff506fa78f9
vorsicht80 <45440187+vorsicht80@users.noreply.github.com>
2023-05-17 08:40:18 +0900

    Update JejuBankApp.java
    for git test

--- jejuBank/src/main/java/com/cqrs/jeju/JejuBankApp.java
+++ jejuBank/src/main/java/com/cqrs/jeju/JejuBankApp.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JejuBankApp {
     public static void main(String[] args) {
         SpringApplication.run(JejuBankApp.class, args);
+        //added comment in jeju bank code for git test
     }
 }
